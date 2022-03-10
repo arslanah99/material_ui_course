224cdfb953c95babf079f2e2278956c65e57e3dd
arslanah99 <42077616+arslanah99@users.noreply.github.com>
2022-03-09 23:02:45 -0700

    installed mui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from '@mui/material';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Button>
+       THIS IS A BUTTON
+     </Button>
+     <AccessibilityIcon />
     </div>
   );
 }
